dc7a77c441ce6b102edcd02c7addde906e243aa2
okay-okay-cyber <meghananeupane@gmail.com>
2024-08-23 21:06:08 +0545

    no need to run npm run dev in server

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,15 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
+            @if (\Session::has('message'))
+            <div class="alert alert-danger">
+                <ul>
+                    <li>{!!  \Session::get('message') !!}</li>
+                </ul>
+            </div>
+            @endif
             <div class="card">
+
                 <div class="card-header">{{ __('Login') }}</div>
 
                 <div class="card-body">
